44eb771b5942f0164a720f117e2692a26e83ecaf
Joz-byte <josephinezelleke@gmail.com>
2023-07-21 11:27:42 +0200

    Added date and month

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -11,7 +11,24 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   let day = days[props.date.getDay()];
+  let month = months[props.date.getMonth()];
+  let monthDay = props.date.getUTCDate();
+
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -23,7 +40,7 @@ export default function FormattedDate(props) {
 
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {monthDay} {month} <br /> {hours}:{minutes}
     </div>
   );
 }
